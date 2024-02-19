45927625d2bfe617ac4a2e9a8b59ae336f77372a
Tiago Vinicius <tiagoviniciusdev@gmail.com>
2024-02-19 14:31:37 -0300

    responsividade

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -11,14 +11,40 @@ import 'swiper/css/navigation';
 // import required modules
 import { Autoplay, Navigation} from 'swiper/modules';
 
+import { useEffect, useState } from 'react';
+
 function Slider() {
+    
+    const [slideNumber, setSliderNumber] = useState(3)
+
+    function sizeOfThings(){
+        if(screen.width > 768){
+            setSliderNumber(3)
+            return
+        } else if(screen.width > 512){
+            setSliderNumber(2)
+            return
+        }  else if(screen.width <= 512){
+            setSliderNumber(1)
+            return
+        }
+      };
+      
+      window.addEventListener('resize', function(){
+        sizeOfThings();
+      });
+
+       useEffect(() => {
+        sizeOfThings()
+       },[])
+
 
 
   return (
     <div className='Slider'>
         <Swiper
         spaceBetween={20}
-        slidesPerView={3}
+        slidesPerView={slideNumber}
         autoplay={{
             delay: 2500,
             disableOnInteraction: false,
@@ -29,28 +55,28 @@ function Slider() {
         className="mySwiper"
         >
             <SwiperSlide>
-                <img src="projectsImg/project01.jpg" alt="" className='slideImg' data-swiper-autoplay="2000"/>
+                <img src="projectsImg/project01.jpg" alt="trabalho recente" className='slideImg' data-swiper-autoplay="2000"/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src="projectsImg/project02.jpg" alt="" className='slideImg' data-swiper-autoplay="2000"/>
+                <img src="projectsImg/project02.jpg" alt="trabalho recente" className='slideImg' data-swiper-autoplay="2000"/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src="projectsImg/project03.jpg" alt="" className='slideImg' data-swiper-autoplay="2000"/>
+                <img src="projectsImg/project03.jpg" alt="trabalho recente" className='slideImg' data-swiper-autoplay="2000"/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src="projectsImg/project04.jpg" alt="" className='slideImg' data-swiper-autoplay="2000"/>
+                <img src="projectsImg/project04.jpg" alt="trabalho recente" className='slideImg' data-swiper-autoplay="2000"/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src="projectsImg/project05.jpg" alt="" className='slideImg' data-swiper-autoplay="2000"/>
+                <img src="projectsImg/project05.jpg" alt="trabalho recente" className='slideImg' data-swiper-autoplay="2000"/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src="projectsImg/project06.jpg" alt="" className='slideImg' data-swiper-autoplay="2000"/>
+                <img src="projectsImg/project06.jpg" alt="trabalho recente" className='slideImg' data-swiper-autoplay="2000"/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src="projectsImg/project07.jpg" alt="" className='slideImg' data-swiper-autoplay="2000"/>
+                <img src="projectsImg/project07.jpg" alt="trabalho recente" className='slideImg' data-swiper-autoplay="2000"/>
             </SwiperSlide>
             <SwiperSlide>
-                <img src="projectsImg/project08.jpg" alt="" className='slideImg' data-swiper-autoplay="2000"/>
+                <img src="projectsImg/project08.jpg" alt="trabalho recente" className='slideImg' data-swiper-autoplay="2000"/>
             </SwiperSlide>
         </Swiper>
     </div>
